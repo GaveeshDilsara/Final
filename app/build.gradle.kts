9f7032b7d07540f6dc689f1992024ae067760f60
Gaveesh <gaveeshrcg@gmail.com>
2024-08-15 12:42:06 +0530

    Finished

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,11 +37,16 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-
-    // Add the Google Play Services Location dependency
-    implementation ("com.google.android.gms:play-services-location:21.0.1")
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation ("com.google.android.gms:play-services-maps:18.0.2")
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
+    implementation ("com.karumi:dexter:6.2.3")
+    implementation ("com.google.android.libraries.places:places:2.6.0")
+
+    implementation ("com.google.android.libraries.places:places:2.6.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 }
--- app/src/main/java/com/example/home/FragmentHome.java
+++ app/src/main/java/com/example/home/FragmentHome.java
@@ -1,19 +1,253 @@
 package com.example.home;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.example.home.network.PlacesService;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class FragmentHome extends Fragment {
+
+    private SupportMapFragment supportMapFragment;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationCallback locationCallback;
+    private GoogleMap googleMap;
+    private Location currentLocation;
+    private PlacesClient placesClient;
+    private PlacesService placesService;
+    private Button btnMyLocation;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
+        Button btnHospitals = rootView.findViewById(R.id.btn_hospitals);
+        Button btnPolice = rootView.findViewById(R.id.btn_police);
+        Button btnPharmacies = rootView.findViewById(R.id.btn_other_places);
+        btnMyLocation = rootView.findViewById(R.id.btn_my_location);
+
+        if (supportMapFragment == null) {
+            Toast.makeText(getActivity(), "Error initializing map fragment", Toast.LENGTH_SHORT).show();
+            return rootView;
+        }
+
+        // Initialize the Places API client
+        Places.initialize(getActivity().getApplicationContext(), "AIzaSyDJ1_q3v8_kgWPZtci0BGZSpUmprdss6F4");
+        placesClient = Places.createClient(getActivity());
+
+        // Initialize Retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://maps.googleapis.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        placesService = retrofit.create(PlacesService.class);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        Dexter.withContext(getActivity()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        getCurrentLocation();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(getActivity(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest request, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+
+        btnHospitals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentLocation != null) {
+                    searchNearbyPlaces("hospital");
+                }
+            }
+        });
+
+        btnPolice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentLocation != null) {
+                    searchNearbyPlaces("police");
+                }
+            }
+        });
+
+        btnPharmacies.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentLocation != null) {
+                    searchNearbyPlaces("pharmacy");
+                }
+            }
+        });
+
+        // Center the map on the user's current location when the button is clicked
+        btnMyLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentLocation != null) {
+                    LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                } else {
+                    Toast.makeText(getActivity(), "Current location not available", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Set up the map and marker click listener
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap map) {
+                googleMap = map;
+
+                // Set a marker click listener
+                googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker) {
+                        LatLng destinationLatLng = marker.getPosition();
+                        String uri = "http://maps.google.com/maps?saddr=" + currentLocation.getLatitude() + "," + currentLocation.getLongitude() + "&daddr=" + destinationLatLng.latitude + "," + destinationLatLng.longitude;
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                        intent.setPackage("com.google.android.apps.maps");
+                        startActivity(intent);
+                        return true;
+                    }
+                });
+            }
+        });
+
+        return rootView;
+    }
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap map) {
+                googleMap = map;
+
+                LocationRequest locationRequest = LocationRequest.create()
+                        .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                        .setInterval(5000)
+                        .setFastestInterval(2000);
+
+                locationCallback = new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        if (locationResult == null) {
+                            return;
+                        }
+                        for (Location location : locationResult.getLocations()) {
+                            currentLocation = location;
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            googleMap.clear();
+                            googleMap.addMarker(new MarkerOptions().position(latLng).title("Current Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                        }
+                    }
+                };
+
+                fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+            }
+        });
+    }
+
+    private void searchNearbyPlaces(String placeType) {
+        LatLng currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        googleMap.clear();
+        googleMap.addMarker(new MarkerOptions().position(currentLatLng).title("Current Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+        String location = currentLocation.getLatitude() + "," + currentLocation.getLongitude();
+
+        placesService.getNearbyPlaces(location, 5000, placeType, "AIzaSyDJ1_q3v8_kgWPZtci0BGZSpUmprdss6F4").enqueue(new Callback<PlacesResponse>() {
+            @Override
+            public void onResponse(Call<PlacesResponse> call, Response<PlacesResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<PlaceResult> places = response.body().results;
+                    for (PlaceResult place : places) {
+                        LatLng latLng = new LatLng(place.geometry.location.lat, place.geometry.location.lng);
+                        googleMap.addMarker(new MarkerOptions().position(latLng).title(place.name));
+                    }
+                } else {
+                    Toast.makeText(getActivity(), "No places found", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PlacesResponse> call, Throwable t) {
+                Toast.makeText(getActivity(), "Request failed: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (fusedLocationProviderClient != null && locationCallback != null) {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        }
     }
 }
